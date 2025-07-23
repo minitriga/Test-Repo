9d09fb1d7c6804f57c25c99c4a33b6b03b8f2215
Alex Gittings <agitting96@gmail.com>
2025-07-23 16:41:10 +0100

    Add support for object files

---
+++ scripts/group.py
@@ -0,0 +1,21 @@
+import logging
+
+from infrahub_sdk import InfrahubClient
+
+
+async def run(
+    client: InfrahubClient,
+    log: logging.Logger,
+    branch: str,
+):
+    log.info(f"Running example script on {branch}...")
+    group = await client.create(kind="CoreStandardGroup", name="Device Group")
+    await group.save(allow_upsert=True)
+
+    device = await client.all(kind="DcimDevice")
+    device = device[0]
+    log.info(f"Got device {device.name.value} with ID {device.id}")
+
+    await group.members.fetch()
+    group.members.add(device)
+    await group.save(allow_upsert=True)
